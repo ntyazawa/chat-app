2db8daa82131360115001f92dde120265eac24b6
ntyazawa <n.yazawa@infomart.co.jp>
2026-02-06 13:42:49 +0900

    プルダウンの実装

--- src/main/java/in/tech_camp/chat_app/form/LoginForm.java
+++ src/main/java/in/tech_camp/chat_app/form/LoginForm.java
@@ -1,5 +1,7 @@
 package in.tech_camp.chat_app.form;
 
+import in.tech_camp.chat_app.validation.ValidationPriority1;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 
@@ -7,6 +9,9 @@ import lombok.Data;
 @Data
 public class LoginForm {
 
+
+  @NotBlank(message = "Name can't be blank", groups = ValidationPriority1.class)
+  private String name;
     private String email;
     private String password;
 }
