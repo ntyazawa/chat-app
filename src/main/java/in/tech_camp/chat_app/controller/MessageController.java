b6ad8d1c56500de21e72e58ca628c53c6299a987
ntyazawa <n.yazawa@infomart.co.jp>
2026-02-09 09:42:44 +0900

    チャットルーム名の表示

--- src/main/java/in/tech_camp/chat_app/controller/MessageController.java
+++ src/main/java/in/tech_camp/chat_app/controller/MessageController.java
@@ -48,6 +48,10 @@ public class MessageController {
     model.addAttribute("messageForm", new MessageForm());
      model.addAttribute("roomId", roomId);
 
+     RoomEntity room = roomRepository.findById(roomId);
+    model.addAttribute("room", room);
+
+
       List<MessageEntity> messages = messageRepository.findByRoomId(roomId);
     model.addAttribute("messages", messages);
 
